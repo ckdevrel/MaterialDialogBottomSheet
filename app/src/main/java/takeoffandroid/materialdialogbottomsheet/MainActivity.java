8eef0d771cb217d32f66d4824af548852946fea7
Chandrasekar K <takeoffandroid@gmail.com>
2015-06-28 17:26:48 +0530

    Initial commit

---
+++ app/src/main/java/takeoffandroid/materialdialogbottomsheet/MainActivity.java
@@ -0,0 +1,81 @@
+package takeoffandroid.materialdialogbottomsheet;
+
+import android.app.Dialog;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openBottomSheet (View v) {
+
+//        int dialogHeight = (int)getDimensPxtoDp(200,MainActivity.getActivity());
+        View view = getLayoutInflater ().inflate (R.layout.bottom_sheet, null);
+        TextView txtExport = (TextView)view.findViewById( R.id.txt_export);
+        TextView txtDetail = (TextView)view.findViewById( R.id.txt_detail);
+        TextView txtOpen = (TextView)view.findViewById( R.id.txt_open);
+        final TextView txtUninstall = (TextView)view.findViewById( R.id.txt_uninstall);
+
+        final Dialog mBottomSheetDialog = new Dialog (MainActivity.this, R.style.MaterialDialogSheet);
+        mBottomSheetDialog.setContentView (view);
+        mBottomSheetDialog.setCancelable (true);
+        mBottomSheetDialog.getWindow ().setLayout (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        mBottomSheetDialog.getWindow ().setGravity (Gravity.BOTTOM);
+        mBottomSheetDialog.show ();
+
+
+        txtExport.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clicked Export",Toast.LENGTH_SHORT).show();
+                mBottomSheetDialog.dismiss();
+            }
+        });
+
+        txtDetail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clicked Detail",Toast.LENGTH_SHORT).show();
+                mBottomSheetDialog.dismiss();
+            }
+        });
+
+        txtOpen.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clicked Open",Toast.LENGTH_SHORT).show();
+                mBottomSheetDialog.dismiss();
+            }
+        });
+
+        txtUninstall.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Clicked Uninstall",Toast.LENGTH_SHORT).show();
+                mBottomSheetDialog.dismiss();
+            }
+        });
+    }
+}
